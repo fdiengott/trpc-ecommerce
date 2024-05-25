8eb1e8aaf476466696c2c1b230eefa53443cfa0c
fdiengott <freddy.diengott@proton.me>
2024-05-25 01:58:18 -0400

    add product router

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,13 +1,12 @@
-/**
- * This file contains the root router of your tRPC-backend
- */
 import { createCallerFactory, publicProcedure, router } from '../trpc';
 import { postRouter } from './post';
+import { productRouter } from './product';
 
 export const appRouter = router({
   healthcheck: publicProcedure.query(() => 'yay!'),
 
   post: postRouter,
+  product: productRouter,
 });
 
 export const createCaller = createCallerFactory(appRouter);
