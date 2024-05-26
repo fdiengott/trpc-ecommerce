0c9f9d89d0bd6e13c78aef90ab82bfdee30462f6
fdiengott <freddy.diengott@proton.me>
2024-05-26 17:51:03 -0400

    move and update product components

---
+++ src/components/Products/Products.tsx
@@ -0,0 +1,18 @@
+import Product from '../Product/Product';
+import { ProductType } from '~/server/routers/product';
+import styles from './products.module.scss';
+
+type ProductsType = {
+  products: ProductType[];
+};
+const Products = ({ products }: ProductsType) => {
+  return (
+    <ul className={styles['products-grid']}>
+      {products?.map((product, i: number) => {
+        return <Product key={`product-${i}`} product={product} />;
+      })}
+    </ul>
+  );
+};
+
+export default Products;
