5ab6e865f37e5a2765b5c47b003db2978efa1cf8
fdiengott <freddy.diengott@proton.me>
2024-06-11 00:28:08 -0400

    update component spacing

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -4,70 +4,70 @@ import Link from 'next/link';
 import LoginLogout from '../LoginLogout';
 
 const Nav = () => {
-  return (
-    <header className={styles.header}>
-      <h1 className="text-4xl font-bold">Finger Lakes Harvest</h1>
-      <nav className="flex items-center space-x-4 md:space-x-6 lg:space-x-8">
-        <Link
-          className="text-sm font-medium hover:underline underline-offset-4"
-          href="/about"
-        >
-          About
-        </Link>
-        <Link
-          className="text-sm font-medium hover:underline underline-offset-4"
-          href="/faqs"
-        >
-          FAQs
-        </Link>
-        <Link
-          className="text-sm font-medium hover:underline underline-offset-4"
-          href="/recipes"
-        >
-          Recipes
-        </Link>
-        <Link
-          className="text-sm font-medium hover:underline underline-offset-4"
-          href="/journal"
-        >
-          Journal
-        </Link>
-        <Link
-          className="text-sm font-medium hover:underline underline-offset-4"
-          href="/contact"
-        >
-          Contact
-        </Link>
-        <Link
-          className="text-sm font-medium hover:underline underline-offset-4"
-          href="/wholesale"
-        >
-          Wholesale
-        </Link>
-        <Link
-          className="text-sm font-medium hover:underline underline-offset-4"
-          href="/testimonials"
-        >
-          Testimonials
-        </Link>
-      </nav>
-      <div
-        className={
-          styles['login-group'] +
-          ' flex items-center space-x-4 md:space-x-6 lg:space-x-8'
-        }
-      >
-        <LoginLogout className="text-sm font-medium hover:underline underline-offset-4" />
-        <Link
-          className="text-sm font-medium hover:underline underline-offset-4"
-          href="#"
-        >
-          <ShoppingCartIcon className="w-5 h-5" />
-          <span className="sr-only">Cart</span>
-        </Link>
-      </div>
-    </header>
-  );
+    return (
+        <header className={styles.header}>
+            <h1 className="text-4xl font-bold">Finger Lakes Harvest</h1>
+            <nav className="flex items-center space-x-4 md:space-x-6 lg:space-x-8">
+                <Link
+                    className="text-sm font-medium hover:underline underline-offset-4"
+                    href="/about"
+                >
+                    About
+                </Link>
+                <Link
+                    className="text-sm font-medium hover:underline underline-offset-4"
+                    href="/faqs"
+                >
+                    FAQs
+                </Link>
+                <Link
+                    className="text-sm font-medium hover:underline underline-offset-4"
+                    href="/recipes"
+                >
+                    Recipes
+                </Link>
+                <Link
+                    className="text-sm font-medium hover:underline underline-offset-4"
+                    href="/journal"
+                >
+                    Journal
+                </Link>
+                <Link
+                    className="text-sm font-medium hover:underline underline-offset-4"
+                    href="/contact"
+                >
+                    Contact
+                </Link>
+                <Link
+                    className="text-sm font-medium hover:underline underline-offset-4"
+                    href="/wholesale"
+                >
+                    Wholesale
+                </Link>
+                <Link
+                    className="text-sm font-medium hover:underline underline-offset-4"
+                    href="/testimonials"
+                >
+                    Testimonials
+                </Link>
+            </nav>
+            <div
+                className={
+                    styles['login-group'] +
+                    ' flex items-center space-x-4 md:space-x-6 lg:space-x-8'
+                }
+            >
+                <LoginLogout className="text-sm font-medium hover:underline underline-offset-4" />
+                <Link
+                    className="text-sm font-medium hover:underline underline-offset-4"
+                    href="#"
+                >
+                    <ShoppingCartIcon className="w-5 h-5" />
+                    <span className="sr-only">Cart</span>
+                </Link>
+            </div>
+        </header>
+    );
 };
 
 export default Nav;
