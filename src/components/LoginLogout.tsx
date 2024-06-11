83d5486c2e86fbd571f4f2097334466037f853b5
fdiengott <freddy.diengott@proton.me>
2024-06-11 00:28:34 -0400

    add loading state to loginlogout

--- src/components/LoginLogout.tsx
+++ src/components/LoginLogout.tsx
@@ -1,33 +1,41 @@
+'use client';
+import styles from './Auth.module.scss';
 import { useSession } from 'next-auth/react';
 import LoginButton from './LoginButton';
 import LogoutButton from './LogoutButton';
 
 const LogWrapper = ({
-  children,
-  className,
+    children,
+    className,
 }: {
-  children: React.ReactNode;
-  className?: string;
+    children: React.ReactNode;
+    className?: string;
 }) => {
-  return <div className={className}>{children}</div>;
+    return (
+        <div className={className + ' ' + styles['auth-btn']}>{children}</div>
+    );
 };
 
 const LoginLogout = ({ className }: { className?: string }) => {
-  const session = useSession();
+    const session = useSession();
 
-  if (session.status === 'authenticated') {
+    if (session.status === 'loading') {
+        return <LogWrapper className="disabled">loading...</LogWrapper>;
+    }
+
+    if (session.status === 'authenticated') {
+        return (
+            <LogWrapper className={className}>
+                <LogoutButton />
+            </LogWrapper>
+        );
+    }
+
     return (
-      <LogWrapper className={className}>
-        <LogoutButton />
-      </LogWrapper>
+        <LogWrapper className={className}>
+            <LoginButton />
+        </LogWrapper>
     );
-  }
-
-  return (
-    <LogWrapper className={className}>
-      <LoginButton />
-    </LogWrapper>
-  );
 };
 
 export default LoginLogout;
