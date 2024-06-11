d74a748b2126b1df06607411f5945a86dfa2df92
fdiengott <freddy.diengott@proton.me>
2024-06-11 00:33:47 -0400

    udpate spacing in product component

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -3,16 +3,19 @@ import { ProductType } from '~/server/routers/product';
 import styles from './products.module.scss';
 
 type ProductsType = {
-  products: ProductType[];
+    products: ProductType[];
 };
 const Products = ({ products }: ProductsType) => {
-  return (
-    <ul className={styles['products-grid']}>
-      {products?.map((product, i: number) => {
-        return <Product key={`product-${i}`} product={product} />;
-      })}
-    </ul>
-  );
+    //  TODO: add pagination? maybe not use useInfiniteQuery
+    // const [pageNum, setPageNum] = useState(0);
+
+    return (
+        <ul className={styles['products-grid']}>
+            {products.map((product, i: number) => {
+                return <Product key={`product-${i}`} product={product} />;
+            })}
+        </ul>
+    );
 };
 
 export default Products;
