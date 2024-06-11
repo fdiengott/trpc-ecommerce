a61718dffa38edf7c05a82a1b742a5652eb185b8
fdiengott <freddy.diengott@proton.me>
2024-06-11 00:19:48 -0400

    make landing page ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,53 +1,52 @@
-import { trpc } from '../utils/trpc';
+import superjson from 'superjson';
+import { createServerSideHelpers } from '@trpc/react-query/server';
 import NextError from 'next/error';
-import type { NextPageWithLayout } from './_app';
+import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { trpc } from '../utils/trpc';
+import { appRouter, createContext } from './api/trpc/[trpc]';
 import Nav from '~/components/Nav/Nav';
 import Products from '../components/Products/Products';
 
+import type { NextPageWithLayout } from './_app';
+import type { ProductType } from '~/server/routers/product';
+
 const IndexPage: NextPageWithLayout = () => {
-  const productQuery = trpc.product.list.useInfiniteQuery(
-    {
-      limit: 10,
-    },
-    {
-      getNextPageParam(lastPage) {
-        return lastPage.nextCursor;
-      },
-    },
-  );
-
-  const { isLoading, data, isError } = productQuery;
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (isError) {
-    return (
-      <NextError
-        title={productQuery.error.message}
-        statusCode={productQuery.error.data?.httpStatus ?? 500}
-      />
+    const productQuery = trpc.product.list.useQuery(
+        { limit: 10 },
+        // {
+        //     getNextPageParam(lastPage) {
+        //         return lastPage.nextCursor;
+        //     },
+        // },
     );
-  }
-  // const utils = trpc.useUtils();
 
-  // prefetch all posts for instant navigation
-  // useEffect(() => {
-  //   const allPosts = postsQuery.data?.pages.flatMap((page) => page.items) ?? [];
-  //   for (const { id } of allPosts) {
-  //     void utils.post.byId.prefetch({ id });
-  //   }
-  // }, [postsQuery.data, utils]);
+    const { isLoading, data, isError } = productQuery;
 
-  return (
-    <div className="flex flex-col bg-gray-800 py-8">
-      <Nav />
-      <section>
-        <Products products={data?.pages[0]?.products} />
-      </section>
-    </div>
-  );
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (isError) {
+        return (
+            <NextError
+                title={productQuery.error.message}
+                statusCode={productQuery.error.data?.httpStatus ?? 500}
+            />
+        );
+    }
+
+    return (
+        <div className="flex flex-col bg-gray-800 py-8">
+            <Head>
+                <title>e-commerce site</title>
+            </Head>
+            <Nav />
+            <section>
+                <Products products={data?.products as ProductType[]} />
+            </section>
+        </div>
+    );
 };
 
 export default IndexPage;
@@ -59,21 +58,39 @@ export default IndexPage;
  *
  * @link https://trpc.io/docs/v11/ssg
  */
-// export const getStaticProps = async (
-//   context: GetStaticPropsContext<{ filter: string }>,
-// ) => {
-//   const ssg = createServerSideHelpers({
-//     router: appRouter,
-//     ctx: await createContext(),
-//   });
+export const getStaticProps: GetStaticProps = async (context) => {
+    const helpers = createServerSideHelpers({
+        router: appRouter,
+        ctx: await createContext(),
+        transformer: superjson,
+    });
+
+    await helpers.product.list.prefetch({});
+
+    return {
+        props: {
+            trpcState: helpers.dehydrate(),
+            filter: context.params?.filter ?? 'all',
+        },
+        revalidate: 1,
+    };
+};
+
+// export const getStaticPaths: GetStaticPaths = async () => {
+//     // const { products } = await appRouter.product.list();
+//     const products = await prisma.product.findMany({
+//         // take: 10,
+//         // orderBy: {
+//         //     createdAt: 'desc',
+//         // },
+//     });
 //
-//   await ssg.post.all.fetch();
+//     const paths = products.map((product) => ({
+//         params: { id: product?.id },
+//     }));
 //
-//   return {
-//     props: {
-//       trpcState: ssg.dehydrate(),
-//       filter: context.params?.filter ?? 'all',
-//     },
-//     revalidate: 1,
-//   };
+//     return {
+//         paths,
+//         fallback: 'blocking',
+//     };
 // };
